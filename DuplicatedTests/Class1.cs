ffbd9955c6bbc81f74d76d48b8ccbb66692acadc
Terje Sandstrom <terje@hermit.no>
2018-03-13 16:59:40 +0100

    Initial code

--- DuplicatedTests/Class1.cs
+++ DuplicatedTests/Class1.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace DuplicatedTests
 {
-    public class Class1
+
+    public enum Season
     {
+        Winter, Spring, Summer, Autoumn
+    }
+
+   
+    public class SUT
+    {
+        [TestCase(Season.Winter,true)]
+        public void ThatEnumsWorks(Season season,bool expected )
+        {
+            Assert.That(expected);
+        }
+
+
+        [TestCase(1, true)]
+        public void ThatIntsWorks(int whatever, bool expected)
+        {
+            Assert.That(expected);
+        }
     }
 }
